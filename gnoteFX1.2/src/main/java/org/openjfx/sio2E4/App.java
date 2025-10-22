138d4e95ada34af053060f97d1625ddf59bd37f2
Foxelou <97977229+Foxelou@users.noreply.github.com>
2025-10-22 16:24:19 +0200

    Amélioration et mise en place du logo

--- gnoteFX1.2/src/main/java/org/openjfx/sio2E4/App.java
+++ gnoteFX1.2/src/main/java/org/openjfx/sio2E4/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.openjfx.sio2E4.service.LocalStorageService;
 
@@ -22,6 +23,7 @@ public class App extends Application {
 
         stage.setTitle("Gnotes");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(App.class.getResourceAsStream("logo.png")));
         stage.setResizable(false);
         stage.show();
     }
