2dc75d24fb1a9ed287c9f9d424c0f366ee9637c6
Foxelou <97977229+Foxelou@users.noreply.github.com>
2025-11-21 09:27:11 +0100

    Ajout de la template de l'api dans le projet Gnotes

---
+++ API_Gnotes/src/main/java/com/stsau/slam2/API_Gnotes/ApiGnotesApplication.java
@@ -0,0 +1,13 @@
+package com.stsau.slam2.API_Gnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGnotesApplication.class, args);
+	}
+
+}
---
+++ API_Gnotes/src/test/java/com/stsau/slam2/API_Gnotes/ApiGnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stsau.slam2.API_Gnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
